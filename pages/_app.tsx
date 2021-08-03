43a692fba2cf9f40e9dd9dc7fd18dbf4df147b19
agustinmarraco <marraco.agustin@gmail>
2021-08-03 00:41:39 -0300

    fixing analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+let gtag: any;
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
